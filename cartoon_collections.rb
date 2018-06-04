18b55cace152b526b760b30d65441ad5a22d3ef5
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-04 18:56:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,6 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_the_cheese(ingredients)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   ingredients.find do |ingredient|
     cheese_types.include?(ingredient)
